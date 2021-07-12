3b9d1198a308e7e13d33735c089ee5cbd6f22493
RedElectricity <redelectricity@unaltered.xyz>
2021-07-12 15:24:07 +0800

    Upload Base

---
+++ TalkBot/__init__.py
@@ -0,0 +1,15 @@
+__version__ = '0.1.0'
+
+'''
+Hi TalkBot~
+Author:RedElectricity
+'''
+
+#导入相关模块
+import fuzzywuzzy #模糊查询
+import jieba #结巴分词
+import pickle #持久化
+import httpx #网络交互
+
+async def Talk(UserID:int ,Message:str ):
+    pass

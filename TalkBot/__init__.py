fa217c9b132d9d5e16f5cd40f4d36f740ed907b5
RedElectricity <redelectricity@unaltered.xyz>
2021-07-12 15:55:52 +0800

    暂时先提交一下

--- TalkBot/__init__.py
+++ TalkBot/__init__.py
@@ -6,10 +6,26 @@ Author:RedElectricity
 '''
 
 #导入相关模块
-import fuzzywuzzy #模糊查询
+from fuzzywuzzy import fuzz #模糊查询
 import jieba #结巴分词
 import pickle #持久化
 import httpx #网络交互
+import yaml #配置文件需要的模块
 
-async def Talk(UserID:int ,Message:str ):
-    pass
+#导入设置
+config = yaml.load(open("config,yaml",'r'),Loader=yaml.FullLoader())
+
+async def Talk(UserID:int ,Message:str):
+    #取出用户数据
+    user_data = pickle.load(open('message.bin','rb'))
+    #检查是否是新用户
+    if UserID in user_data:
+        if len(user_data[UserID]) > 1:
+            #判断是否是已经回答过的问题
+            if fuzz(Message,user_data[UserID][len(user_data[UserID])-1]) >= 80:
+               return config['Wording']['dwta'] 
+            else:
+                #这里是回答参数
+                pass
+    else:
+        pass
